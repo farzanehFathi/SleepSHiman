a58070fa5adc232204694bade1a364f8ecc7d342
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-04-24 22:49:35 +0330

    created navbar and top cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+      colors: {
+        branding: "#0053f7",
+        gray: {
+          10: "#E8E8E8",
+          20: "#fbfbfb",
+          30: "#939393",
+
+          100: "#4f4f4f",
+          200: "#2c2c2c",
+        },
+        accent: {
+          primary: {
+            100: "#ffa229",
+            200: "#ff8900",
+          },
+          secondary: { 100: "#45d541", 200: "#42d43a" },
+        },
+      },
+      fontFamily: {
+        body: ["Vazirmatn"],
+      },
+    },
   },
   plugins: [],
 };
