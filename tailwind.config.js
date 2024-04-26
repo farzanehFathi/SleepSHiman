300fccced272ce08d89b20fbdbd74c438c4f6c0a
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-04-26 16:19:13 +0330

    Done with large screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,19 +12,26 @@ module.exports = {
         },
       },
 
+      fontSize: {
+        nuno: ["6px", "6px"],
+      },
+
       spacing: {
+        0.5: "1px",
         128: "32rem",
       },
       colors: {
         branding: "#0053f7",
         gray: {
-          10: "#E8E8E8",
-          20: "#fbfbfb",
+          5: "#F7F7F7",
+          10: "#B3B3B3",
+          20: "#E9E9E9",
           30: "#939393",
 
           100: "#4f4f4f",
           200: "#2c2c2c",
         },
+        red: { 10: "#FF004F" },
         accent: {
           primary: {
             100: "#ffa229",
